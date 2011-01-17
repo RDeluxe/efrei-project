513aa2efb8f72d086a94bfa4b886f7af39ad8170
benjamin.bozou <benjamin.bozou@cfece211-60d3-e765-60b0-59dd529983e2>
2011-01-17 23:02:18 +0000

    New release of the diagrams and the class

--- trunk/ArtISCode/src/domain/User.java
+++ trunk/ArtISCode/src/domain/User.java
@@ -1,41 +1,174 @@
+/**
+ * 
+ */
 package domain;
 
+/** 
+ * <!-- begin-UML-doc -->
+ * <!-- end-UML-doc -->
+ * @author Benjamin BOZOU
+ * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ */
 public class User {
-
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
 	private long id;
-	private String firstname;
-	private String lastname;
-	private String password;
-	
-	
-	void setID(long id) {
-		this.id = id;
-	}
-	long getID() {
+
+	/** 
+	 * @return id
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public long getId() {
+		// begin-user-code
 		return id;
+		// end-user-code
 	}
 
-	void setFirstName(String firstname) {
-		this.firstname = firstname;
+	/** 
+	 * @param id id à définir
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setId(long id) {
+		// begin-user-code
+		this.id = id;
+		// end-user-code
 	}
-	String getFirstName() {
+
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private String login;
+
+	/** 
+	 * @return login
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public String getLogin() {
+		// begin-user-code
+		return login;
+		// end-user-code
+	}
+
+	/** 
+	 * @param login login à définir
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setLogin(String login) {
+		// begin-user-code
+		this.login = login;
+		// end-user-code
+	}
+
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private String firstname;
+
+	/** 
+	 * @return firstname
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public String getFirstname() {
+		// begin-user-code
 		return firstname;
+		// end-user-code
 	}
-	
-	void setLastName(String lastname) {
-		this.lastname = lastname;
+
+	/** 
+	 * @param firstname firstname à définir
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setFirstname(String firstname) {
+		// begin-user-code
+		this.firstname = firstname;
+		// end-user-code
 	}
-	String getLastName() {
+
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private String lastname;
+
+	/** 
+	 * @return lastname
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public String getLastname() {
+		// begin-user-code
 		return lastname;
+		// end-user-code
 	}
 
-	void setPassword(String password) {
-		this.password = password;
+	/** 
+	 * @param lastname lastname à définir
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setLastname(String lastname) {
+		// begin-user-code
+		this.lastname = lastname;
+		// end-user-code
 	}
-	String getPassword() {
+
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private String password;
+
+	/** 
+	 * @return password
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public String getPassword() {
+		// begin-user-code
 		return password;
-		
+		// end-user-code
 	}
 
+	/** 
+	 * @param password password à définir
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setPassword(String password) {
+		// begin-user-code
+		this.password = password;
+		// end-user-code
+	}
 
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Address address;
+
+	/** 
+	 * @return address
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Address getAddress() {
+		// begin-user-code
+		return address;
+		// end-user-code
+	}
+
+	/** 
+	 * @param address address à définir
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setAddress(Address address) {
+		// begin-user-code
+		this.address = address;
+		// end-user-code
+	}
 }

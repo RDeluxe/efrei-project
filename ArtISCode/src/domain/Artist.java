16e10608d499eba85086957f944c203a5d0c8468
benjamin.bozou <benjamin.bozou@cfece211-60d3-e765-60b0-59dd529983e2>
2011-01-17 23:02:18 +0000

    New release of the diagrams and the class

--- ArtISCode/src/domain/Artist.java
+++ ArtISCode/src/domain/Artist.java
@@ -1,64 +1,95 @@
+/**
+ * 
+ */
 package domain;
 
-public class Artist {
-	
-	private long id;
-	private String firstname;
-	private String lastname;
-	private String password;
+import java.util.Set;
+
+/** 
+ * <!-- begin-UML-doc -->
+ * <!-- end-UML-doc -->
+ * @author Benjamin BOZOU
+ * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ */
+public class Artist extends User {
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
 	private String email;
-	private String type;
-	private String description;
-	
-	public void setID(long id) {
-		this.id = id;
-	}
-	public long getID() {
-		return id;
-	}
-	
-	void setFirstName(String firstname) {
-		this.firstname = firstname;
-	}
-	String getFirstName() {
-		return firstname;
-	}
-	
-	void setLastName(String lastname) {
-		this.lastname = lastname;
-	}
-	String getLastName() {
-		return lastname;
+
+	/** 
+	 * @return email
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public String getEmail() {
+		// begin-user-code
+		return email;
+		// end-user-code
 	}
 
-	void setPassword(String password) {
-		this.password = password;
-	}
-	String getPassword() {
-		return password;
-		
-	}
-	
+	/** 
+	 * @param email email à définir
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
 	public void setEmail(String email) {
+		// begin-user-code
 		this.email = email;
+		// end-user-code
 	}
-	public String getEmail() {
-		return email;
-	}
-	
-	public void setType(String type) {
-		this.type = type;
-	}
-	public String getType() {
-		return type;
-	}
-	
-	public void setDescription(String description) {
-		this.description = description;
-	}
+
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private String description;
+
+	/** 
+	 * @return description
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
 	public String getDescription() {
+		// begin-user-code
 		return description;
+		// end-user-code
 	}
-	
-	
+
+	/** 
+	 * @param description description à définir
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setDescription(String description) {
+		// begin-user-code
+		this.description = description;
+		// end-user-code
+	}
+
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Set<Tag> tag;
+
+	/** 
+	 * @return tag
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Set<Tag> getTag() {
+		// begin-user-code
+		return tag;
+		// end-user-code
+	}
+
+	/** 
+	 * @param tag tag à définir
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setTag(Set<Tag> tag) {
+		// begin-user-code
+		this.tag = tag;
+		// end-user-code
+	}
 }
